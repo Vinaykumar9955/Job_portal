589f0dcf94b384f04a42fca583092934a8206b4e
Your Name <your_email@example.com>
2024-11-21 23:02:13 +0530

    corrected error

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin: 'https://job-portal-j3.vercel.app/', 
+    origin: 'https://job-portal-j3.vercel.app', 
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
     allowedHeaders: ['Content-Type', 'Authorization'], 
     credentials: true, // Allow cookies and credentials
