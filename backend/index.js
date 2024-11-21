0dd2d630c9df82c307411f281c86fe363240f0b3
Vinay Kumar <147934708+Vinaykumar9955@users.noreply.github.com>
2024-11-21 23:49:43 +0530

    Update cors

--- backend/index.js
+++ backend/index.js
@@ -17,13 +17,22 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
+const allowedOrigins = ['http://localhost:5173', 'https://job-portal-j3.vercel.app',"https://job-portal-j3.vercel.app"];
+
 const corsOptions = {
-    origin: 'https://job-portal-j3.vercel.app', 
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
-    allowedHeaders: ['Content-Type', 'Authorization'], 
-    credentials: true, // Allow cookies and credentials
-  };
+    origin: (origin, callback) => {
+        if (allowedOrigins.includes(origin) || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+};
 app.use(cors(corsOptions));
+
 
 const PORT = process.env.PORT ||  3000;
 //api's
